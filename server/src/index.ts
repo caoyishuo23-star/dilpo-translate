6590d57bac11e202d15ee3aeae4e939d5791a794
user8888414619 <3748669149688219-user8888414619@noreply.coze.cn>
2026-04-12 13:34:02 +0800

    fix: update backend URL to Zeabur

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,6 +26,6 @@ app.get('/api/v1/health', (req, res) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}/`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening at http://0.0.0.0:${port}/`);
 });
